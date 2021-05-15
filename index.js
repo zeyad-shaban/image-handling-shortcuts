44341d42acb9bafc6de3a9705961e2e8a213f720
zeyadShapan <zeyadshapan2004@gmail.com>
2021-05-15 14:05:31 +0200

    Added copyImageAddress to clipboard

--- index.js
+++ index.js
@@ -6,24 +6,45 @@ document.onmousemove = e => {
     cursorY = e.clientY;
 };
 
+const getNearestImage = () => document.elementFromPoint(cursorX, cursorY);
 
-const downloadFile = async (src) => {
-    const filename = src.replace(/^.*[\\\/]/, '');
+
+const saveImageAs = () => {
+    const img = getNearestImage();
+    const src = img.getAttribute('src');
+    const imgName = src.replace(/^.*[\\\/]/, '');
 
     var a = document.createElement('a');
     a.href = src;
-    a.download = filename;
-    // document.body.appendChild(a);
+    a.download = imgName;
     a.click();
-    // document.body.removeChild(a);
 };
 
+const copyImageAddress = () => {
+    try {
+        const img = getNearestImage();
+        const src = img.getAttribute('src');
 
-const saveImgAs = () => {
-    const targetImg = document.elementFromPoint(cursorX, cursorY);
-    downloadFile(targetImg.getAttribute('src'));
+        const copyText = document.createElement("input");
+        copyText.setAttribute('value', src);
+        document.body.appendChild(copyText);
+
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+
+        document.body.removeChild(copyText);
+        // Todo add little sound to notify user
+    } catch (err) {
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", src);
+    }
 };
+
 
 window.onkeypress = e => {
-    if (e.shiftKey && e.key.toLowerCase() === 's') saveImgAs();
+    const key = e.key.toLowerCase();
+    const firstPart = e.shiftKey && (e.ctrlKey || e.metaKey);
+
+    if (firstPart && key === 's') saveImageAs();
+    else if (firstPart && key === 'c') copyImageAddress();
 };
